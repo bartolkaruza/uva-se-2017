dfa1aa8ec0515b77abcd28ad20aebdb9cc11a712
Bartol Karuza <bartol@immidi.com>
2017-11-18 14:05:20 +0100

    test data added to regression-set for LOC test

---
+++ example/src/regression/LinesOfCodeVariants.java
@@ -0,0 +1,75 @@
+package regression;
+
+/**
+ * Class documentation
+ */
+public abstract class LinesOfCodeVariants {
+	
+	public LinesOfCodeVariants () {
+		System.out.println("1");
+		System.out.println("2");
+	}
+	
+	public abstract void abstractMethod();
+	
+	public static void staticMethod() {
+		System.out.println("1");
+		System.out.println("2");
+	}
+	
+	// some comment
+	public void commentStyle1() {
+		System.out.println("1");
+		System.out.println("2");
+	}
+	
+	// some comment //
+	public void commentStyle2() {
+		System.out.println("1");
+		System.out.println("2");
+	}
+	
+	/**
+	 * some comment
+	 */
+	public void commentStyle3() {
+		System.out.println("1");
+		System.out.println("2");
+	}
+	
+	public void commentStyle4() {
+		System.out.println("1");
+		// comment
+		System.out.println("2");
+	}
+	
+	public void commentStyle5() {
+		System.out.println("1");
+		/**
+		 * some comment 
+		 */
+		System.out.println("2");
+	}
+	
+	public void commentStyle6() {
+		System.out.println("1"); // comment
+		System.out.println("2");
+	}
+	
+	static class InnerClass {
+		InnerClass() {
+			System.out.println("1");
+			System.out.println("2");
+		}
+	}
+	
+	public void methodVariant1() {
+		System.out.println("1");
+	}
+	
+	public void methodVariant2() {
+		System.out.println("text on multiple "
+				+ "lines");
+	}
+	
+}
