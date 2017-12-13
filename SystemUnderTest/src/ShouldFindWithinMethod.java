fc5f0c88c81ff5ed5db1eb16952901e04c6f2475
Bartol Karuza <bartol@immidi.com>
2017-12-13 14:51:38 +0100

    add missing UT file

---
+++ SystemUnderTest/src/ShouldFindWithinMethod.java
@@ -0,0 +1,18 @@
+public class ShouldFindWithinMethod {
+	
+	public ShouldFindWithinMethod() {
+		int x = 0;
+		int y = 5;
+		int z = 3;
+		
+		z = x + y;
+		z++;
+		
+		y = x + 5;
+		
+		z = x + y;
+		z++;
+		
+	}
+
+}
